26eaf2a1eb4b05b3295156011c8a401cd2697555
Martin Bukatovic <mbukatov@redhat.com>
2017-05-18 19:25:53 +0200

    use user site-packages modules
    Without this, Sphinx would not be able to import pylatest module
    installed via `pip install --user pylatest`.

--- conf.py
+++ conf.py
@@ -15,11 +15,13 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
+import os
+import sys
+
+# try to use pylatest installed via `pip install --user pylatest`
+if sys.version_info >= (2, 7):
+    sys.path.insert(0, os.path.expanduser('~/.local/lib/python2.7/site-packages'))
 
 # -- General configuration ------------------------------------------------
 
